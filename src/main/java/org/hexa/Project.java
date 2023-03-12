af502d6fef65d83d3f33631b108397b0de7a5d05
DhanalakshmiMurugan02 <dhanalakshmi.murugan@gmail.com>
2023-03-12 17:06:17 +0530

    initial codes

---
+++ src/main/java/org/hexa/Project.java
@@ -0,0 +1,15 @@
+package org.hexa;
+
+public class Project {
+	private void tamil() {
+		System.out.println("1");
+	}
+
+	private void english() {
+		System.out.println("2");
+	}
+
+	private void telugu() {
+		System.out.println("3");
+	}
+}
